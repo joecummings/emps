ff45bcda94d7b943fee41e5e9f75b5c7a2bbb716
Michael Huyler <michaelhuyler@gmail.com>
2019-06-04 22:30:13 -0500

    Custom number of 5x5 to generate

--- gym_maze/envs/maze_generator.py
+++ gym_maze/envs/maze_generator.py
@@ -9,17 +9,20 @@ if __name__ == "__main__":
         # create it if it doesn't
         os.mkdir(dir_name)
 
-    # increment number until it finds a name that is not being used already (max maze_999)
-    maze_path = None
-    for i in range(1, 1000):
-        maze_name = "maze2d_%03d.npy" % i
-        maze_path = os.path.join(dir_name, maze_name)
-        if not os.path.exists(maze_path):
-            break
-        if i == 999:
-            raise ValueError("There are already 999 mazes in the %s." % dir_name)
+    mazes_to_make = int(input("How many mazes to generate: "))
+    maze_count = 0
+    while maze_count < mazes_to_make:
+        maze_count = maze_count + 1
+        # increment number until it finds a name that is not being used already (max maze_999)
+        maze_path = None
+        for i in range(1, 1000):
+            maze_name = "maze2d_%03d.npy" % i
+            maze_path = os.path.join(dir_name, maze_name)
+            if not os.path.exists(maze_path):
+                break
+            if i == 999:
+                raise ValueError("There are already 999 mazes in the %s." % dir_name)
 
-    maze = Maze(maze_size=(5, 5), num_reward_tiles=3)
-    maze.save_maze(maze_path)
-    print("New maze generated and saved at %s." % maze_path)
-
+        maze = Maze(maze_size=(5, 5), num_reward_tiles=3)
+        maze.save_maze(maze_path)
+        print("New maze generated and saved at %s." % maze_path)
