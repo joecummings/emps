ddec7bda217e9a2fd604b4225b82340e74f5389c
Joe Cummings <jrcummings27@gmail.com>
2019-06-09 13:00:28 -0500

    asdf

--- main.py
+++ main.py
@@ -8,24 +8,21 @@ import gym_maze
 from gym_maze.envs.maze_view_2d import Maze, MazeView2D
 from gym_maze.envs.maze_env import max_maze_file_digit
 
-
 curr_state = np.asarray([0.5, 0.5, 0.5, 0.5])  #[happy, sad, bored, frustrated]
 mood = np.asarray([0.5, 0.5, 0.5, 0.5])
-curr_emotion = curr_state.index(max(curr_state))
+curr_emotion = 0
 
 def simulate():
 
     # Instantiating the learning related parameters
     learning_rate = get_learning_rate(0)
     explore_rate = get_explore_rate(0)
-    discount_rate = get_discount_rate(0)
+    discount_rate = 0.99
 
     num_streaks = 0
 
     # Render tha maze
     env.render()
-
-    prev_rewards = []
 
     for episode in range(NUM_EPISODES):
 
@@ -36,12 +33,14 @@ def simulate():
         state_0 = state_to_bucket(obv)
         total_reward = 0
 
-        for t in range(MAX_T):
+        prev_state = []
 
-            # Update parameters
-            explore_rate = get_explore_rate(t)
-            learning_rate = get_learning_rate(t)
-            # discount_rate += get_discount_rate(t)
+        curr_state = np.asarray([0.5, 0.5, 0.5, 0.5])  #[happy, sad, bored, frustrated]
+        mood = np.asarray([0.5, 0.5, 0.5, 0.5])
+        curr_emotion = 0
+
+        for t in range(MAX_T):
+            curr_emotion = np.where(curr_state == max(curr_state))[0][0]
 
             # Select an action
             action = select_action(state_0, explore_rate)
@@ -52,11 +51,6 @@ def simulate():
             # Observe the result
             state = state_to_bucket(obv)
             total_reward += reward
-            prev_rewards.insert(0, reward)
-
-            # update emotion state
-            if curr_state.index(max(curr_state)) != curr_emotion:
-                update_emotion_state(t, prev_rewards)
 
             # Update the Q based on the result
             best_q = np.amax(q_table[state])
@@ -65,40 +59,13 @@ def simulate():
             # Setting up for the next iteration
             state_0 = state
 
-            # Print data
-            if DEBUG_MODE > 1:
-                print("\nEpisode = %d" % episode)
-                print("t = %d" % t)
-                print("Action: %d" % action)
-                print("State: %s" % str(state))
-                print("Reward: %f" % reward)
-                print("Total Reward: %f" % total_reward)
-                print("Best Q: %f" % best_q)
-                print("Explore rate: %f" % explore_rate)
-                print("Learning rate: %f" % learning_rate)
-                print("Discount rate:", discount_rate)
-                print("Q: ", q_table)
-                print("Streaks: %d" % num_streaks)
-                print("Total reward: %f" % total_reward)
-                print("")
-
-            if DEBUG_MODE > 0:
-                if done or t >= MAX_T - 1:
-                    print("\nEpisode = %d" % episode)
-                    print("t = %d" % t)
-                    print("Explore rate: %f" % explore_rate)
-                    print("Learning rate: %f" % learning_rate)
-                    print("Streaks: %d" % num_streaks)
-                    print("Total reward: %f" % total_reward)
-                    print("")
-
             # Render tha maze
             if RENDER_MAZE:
                 env.render()
 
             if env.is_game_over():
-                return
-                # sys.exit()
+                # return
+                sys.exit()
 
             if done:
                 print("Episode %d finished after %f time steps with total reward = %f (streak %d)."
@@ -114,11 +81,22 @@ def simulate():
                 print("Episode %d timed out at %d with total reward = %f."
                       % (episode, t, total_reward))
 
-        if DEBUG_MODE > 2:
-            wait = input("Press enter to continue to next episode...")
+            # update emotion state and mood
+            curr = q_table[state_0 + (action,)]
+            prev_state.insert(0, curr)
+
+            curr_state = update_emotion_state(t, prev_state)
+            curr_state = list(map( lambda x,y : min([(x+y)/2, 1.0]), curr_state, mood ))
+
+            mood = np.asarray(curr_state)
+
+            # print("emotions: ", curr_state, np.where(curr_state == max(curr_state))[0][0], ", ", curr_emotion)
+            if np.where(curr_state == max(curr_state))[0][0] != curr_emotion:
+                explore_rate = get_explore_rate(episode)
+                learning_rate = get_learning_rate(episode)           
 
         # It's considered done when it's solved over 120 times consecutively
-        if num_streaks > STREAK_TO_END or True:
+        if num_streaks > STREAK_TO_END:
             break
 
 
@@ -132,44 +110,49 @@ def select_action(state, explore_rate):
     return action
 
 def update_emotion_state(t, prev_reward):
-    delta = 0.0
-    
+
     try:
-        delta = abs(prev_reward[0]/prev_reward[1])
-        
-        if prev_reward[1] > prev_reward[0]:
-            delta = delta * -1.0
-        
+        delta = prev_reward[0] - prev_reward[1]
     except:
-       delta = 0.0
-
-    if delta > 2.0:
-        curr_state[0] = min(curr_state[0] + 0.1, 1.0) #happiness goes up
-        curr_state[1] = max(curr_state[1] - 0.1, 0.0) #sadness goes down
-        curr_state[2] = max(curr_state[2] - 0.1, 0.0) #boredom goes down
-        curr_state[3] = max(curr_state[3] - 0.1, 0.0) #frustration goes down
+        delta = 0.0
     
-    elif delta < 2.0:
-        curr_state[0] =  max(curr_state[0] - 0.1, 0.0) #happiness goes down
-        curr_state[1] = min(curr_state[1] + 0.1, 1.0) #sadness goes up
-        curr_state[2] = max(curr_state[2] - 0.1, 0.0) #boredom goes down
+    # print(delta, curr_state)
+
+    if delta > 0.02:
+        curr_state[0] = min(curr_state[0] + 0.05, 1.0) #happiness goes up
+        curr_state[1] = max(curr_state[1] - 0.05, 0.0) #sadness goes down
+        curr_state[2] = max(curr_state[2] - 0.05, 0.0) #boredom goes down
+        curr_state[3] = max(curr_state[3] - 0.05, 0.0) #frustration goes down
     
-    else:
+    elif delta < -0.02:
+        curr_state[0] =  max(curr_state[0] - 0.05, 0.0) #happiness goes down
+        curr_state[1] = min(curr_state[1] + 0.05, 1.0) #sadness goes up
+        curr_state[2] = max(curr_state[2] - 0.05, 0.0) #boredom goes down
 
+    else: # == 0
+        curr_state[3] = min(curr_state[3] + 0.05, 1.0) #frustration goes up
+        curr_state[2] = min(curr_state[2] + 0.05, 1.0) #boredom goes up
+        curr_state[0] = max(curr_state[0] - 0.05, 0.0) #happiness goes down
+
+    return curr_state
     
-
-
 
 def get_explore_rate(er):
-    const_emo_er = [-0.20, -0.10, 0.10, 0.10]
+    if er == 0:
+        return 0.5
 
-    return max(MIN_EXPLORE_RATE, min(0.8, 1.0 - np.dot(const_emo_er, curr_state)))
+    const_emo_er = [-0.20, -0.10, 0.40, 0.40]
+
+    return max(MIN_EXPLORE_RATE, min(0.8, np.dot(const_emo_er, curr_state)))
 
 
 def get_learning_rate(t):
-    const_emo_lr = [0.2, -0.1, -0.1, 0.1]
+    if t == 0:
+        return 0.5
 
-    return max(MIN_EXPLORE_RATE, min(0.8, 1.0 - np.dot(const_emo_lr, curr_state)))
+    const_emo_lr = [0.5, -0.4, -0.4, 0.3]
+
+    return max(MIN_LEARNING_RATE, min(0.8, np.dot(const_emo_lr, curr_state)))
 
 
 def state_to_bucket(state):
@@ -228,7 +211,7 @@ if __name__ == '__main__':
         MAX_T = np.prod(MAZE_SIZE, dtype=int) * 100
         STREAK_TO_END = 100
         SOLVED_T = np.prod(MAZE_SIZE, dtype=int)
-        DEBUG_MODE = 2  # [0...3]; less <---> more verbose
+        DEBUG_MODE = 0 # [0...3]; less <---> more verbose
         RENDER_MAZE = True
         ENABLE_RECORDING = True
 
